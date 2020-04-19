63339796fc6f0491f269808f219ba26d481da719
Eugene Puntus <63738231+JakkoPuntus@users.noreply.github.com>
2020-04-19 03:55:20 +0300

    Bot was created

---
+++ Bot/bot.py
@@ -0,0 +1,34 @@
+import telebot
+import config
+import weather
+from telebot import types
+import pyketiki
+
+
+bot = telebot.TeleBot('964615920:AAEwHybjIse8w7tBe76kuMNBZpo6ckO23vg')
+markup = types.ReplyKeyboardMarkup()
+markup.row('коронавирус', 'погода')
+markup.row('пикеты')
+
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, 'выберите команду', reply_markup=markup)
+
+@bot.message_handler(regexp="коронавирус")
+def handle_message(message):
+    bot.send_message(message.chat.id, config.Count, reply_markup=markup)
+    bot.send_message(message.chat.id, config.CountNF, reply_markup=markup)
+
+@bot.message_handler(regexp="погода")
+def handle_message(message):
+    bot.send_message(message.chat.id, weather.WeatherToday, reply_markup=markup)    
+    bot.send_message(message.chat.id, weather.Weather, reply_markup=markup)    
+    
+@bot.message_handler(regexp="пикеты")
+def handle_message(message):
+    bot.send_message(message.chat.id, pyketiki.piket, reply_markup=markup)
+    
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
